d0552d5f8fae121ed5fe1011a394ee0d08502d69
Robby Widyahartono <robby.widyahartono@gmail.com>
2025-01-01 20:56:23 +0700

    update back button

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,19 +1,8 @@
-"use client";
-import { useRouter } from 'next/navigation';
-
+import Link from 'next/link';
 export function BackButton() {
-  const router = useRouter();
-
-  const handleBackClick = () => {
-    router.back(); // Navigate back to the previous page
-  };
-
   return (
-    <button
-      onClick={handleBackClick}
-      className="mt-10 bg-gray-800 text-white py-2 px-4 rounded hover:bg-gray-700 transition-all"
-    >
+    <Link  className="mt-10 bg-gray-800 text-white py-2 px-4 rounded hover:bg-gray-700 transition-all" href="/">
       Back
-    </button>
+    </Link>
   );
 }
